99e79aec27254cc1b6f0f8502a566f930905ec62
摸鱼玩家 <power916@163.com>
2024-05-05 18:17:08 +0800

    1

--- js/48.js
+++ js/48.js
@@ -6,7 +6,15 @@ let is_weixin_qq = (function() {
     }else
     if (ua.match(/QQ/i) == 'qq') {
         //如果是QQ则打开此链接   ！！按照书写标准写入到"内！！
+<<<<<<< Updated upstream
         window.location.href = "http://qq.com"
+=======
+<<<<<<< HEAD:js/48.js
+        window.location.href = "http://qq.com"
+=======
+        window.location.href = "http://www.xiyuhh678.cn/ewmdy/"
+>>>>>>> fe3aa0ac8c9bddf5a95e8fb0161f6944b15e6dc0:js/43.js
+>>>>>>> Stashed changes
     } else {
         //如果不是微信也不是QQ则打开此链接   ！！按照书写标准写入到"内！！
         window.location.href = "http://www.gov.cn"
--- js/51.js
+++ js/51.js
@@ -6,7 +6,27 @@ let is_weixin_qq = (function() {
     }else
     if (ua.match(/QQ/i) == 'qq') {
         //如果是QQ则打开此链接   ！！按照书写标准写入到"内！！
+<<<<<<< Updated upstream
+<<<<<<<< Updated upstream:js/51.js
         window.location.href = "http://qq.com"
+========
+<<<<<<< HEAD:js/49.js
+        window.location.href = "http://qq.com"
+=======
+        window.location.href = "http://www.xiyuhh678.cn/ewmdy/"
+>>>>>>> fe3aa0ac8c9bddf5a95e8fb0161f6944b15e6dc0:js/powers-34.js
+>>>>>>>> Stashed changes:js/49.js
+=======
+<<<<<<<< Updated upstream:js/50.js
+        window.location.href = "http://qq.com"
+========
+<<<<<<< HEAD:js/51.js
+        window.location.href = "http://qq.com"
+=======
+        window.location.href = "http://www.xiyuhh678.cn/ewmdy/"
+>>>>>>> fe3aa0ac8c9bddf5a95e8fb0161f6944b15e6dc0:js/yannmo27.js
+>>>>>>>> Stashed changes:js/51.js
+>>>>>>> Stashed changes
     } else {
         //如果不是微信也不是QQ则打开此链接   ！！按照书写标准写入到"内！！
         window.location.href = "http://www.gov.cn"
